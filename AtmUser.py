aff3187de062a61fc9126f1bd66d1a09f9a46315
Kevin-Sambuli <sambulikevin@gmail.com>
2019-10-25 20:39:23 +0300

    python programming

--- AtmUser.py
+++ AtmUser.py
@@ -3,8 +3,10 @@ from ATM import ATMCard
 
 kevinCard = ATMCard()
 sambuliCard = ATMCard()
+AmuhayaCard = ATMCard
 
 print(type(kevinCard))
 
 print(kevinCard.name)
 print(sambuliCard.color)
+print(AmuhayaCard.)
